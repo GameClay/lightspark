b55b01a3905b29f15fadab07f2cb585e89cab58c
Pat Wilson <zerostride@gmail.com>
2010-08-18 16:32:06 -0700

    Added better error reporting
    More information when a class name collision happens.

--- scripting/class.h
+++ scripting/class.h
@@ -51,7 +51,11 @@ public:
 	Class_inherit(const tiny_string& name):Class_base(name),tag(NULL)
 	{
 		bool ret=sys->classes.insert(std::make_pair(name,this)).second;
-		assert_and_throw(ret && "Class name collision found");
+		if(!ret)
+		{
+			LOG(LOG_ERROR,"Class name collision at: " << name);
+			throw RunTimeException("Class name collision");
+		}
 	}
 	void buildInstanceTraits(ASObject* o) const;
 	void bindTag(DictionaryTag const* t)
